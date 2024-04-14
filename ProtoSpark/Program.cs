d4b827e546447907c2c67d28643dfcdb679537c2
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2024-04-14 15:43:37 -0400

    Demo C# project

---
+++ ProtoSpark/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProtoSpark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
