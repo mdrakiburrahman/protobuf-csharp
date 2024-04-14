42eef944b159525a940524e24f244a8aae5388b6
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2024-04-14 16:12:34 -0400

    Proto R/W

--- ProtoSpark/Program.cs
+++ ProtoSpark/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace ProtoSpark
+﻿using Google.Protobuf;
+
+namespace ProtoSpark
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // creating an instance of Employee
+            Employee employee = new Employee()
+            {
+                Id = 1,
+                FullName = "john smith",
+                Email = "johnsmith@gmail.com"
+            };
+
+            //store bytes
+            byte[] employeeBytes;
+
+            // Write to a stream
+            using (MemoryStream stream = new MemoryStream())
+            {
+                employee.WriteTo(stream);
+                employeeBytes = stream.ToArray();
+            }
+            // Read from a stream bytes
+            Employee.Parser.ParseFrom(employeeBytes);
+
+            // Write to a file
+            using (Stream output = File.OpenWrite("mydata.data"))
+            {
+                employee.WriteTo(output);
+            }
+
+            // Read from a data file
+            using (Stream output = File.OpenRead("mydata.data"))
+            {
+                var employeeFromFile = Employee.Parser.ParseFrom(output);
+            }
         }
     }
 }
